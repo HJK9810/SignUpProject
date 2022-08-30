6b37672ca498cc5072c5916e8efb2d226de44470
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-08-30 10:26:58 +0900

    remove Unnecessary

--- src/main/java/kr/ac/kopo/ctc/kopo12/domain/Member.java
+++ src/main/java/kr/ac/kopo/ctc/kopo12/domain/Member.java
@@ -4,15 +4,12 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Member {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column
 	private String id;
 	@Column
